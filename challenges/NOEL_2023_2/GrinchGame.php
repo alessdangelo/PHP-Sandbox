a587e67b21c413b8cee6092bc16f658e687169d7
aless <61836900+alessdangelo@users.noreply.github.com>
2024-05-28 11:31:41 +0200

    remove foreach loops by using collections

--- challenges/NOEL_2023_2/GrinchGame.php
+++ challenges/NOEL_2023_2/GrinchGame.php
@@ -4,6 +4,7 @@ namespace Challenges\NOEL_2023_2;
 
 use Challenges\NOEL_2023_2\Kid;
 use Challenges\NOEL_2023_2\Grinch;
+use Illuminate\Support\Collection;
 
 class GrinchGame
 {
@@ -11,43 +12,41 @@ class GrinchGame
     private const TIME_LOST = 5;
     public string $answer = '';
     public Grinch $grinch;
-    public array $kidsCollection = [];
+    public Collection $kidsCollection;
 
     public function __construct(public int $time, public array $kids, public int $grinchFear)
     {
-        $this->kidsCollection = $this->formatKids($kids);
+        $this->kidsCollection = collect($this->formatKids($kids));
         $this->grinch = new Grinch($grinchFear);
     }
 
-    private function formatKids($kidsCodeArray): array
+    private function formatKids($kidsEncodedCollection): array
     {
-        $kids = [];
-        foreach ($kidsCodeArray as $kidCode) {
-            $kids[] = Kid::generateKid($kidCode);
-        }
-
-        return $kids;
+        $kids = collect($kidsEncodedCollection);
+        $kids->transform(function ($item) {
+            return Kid::generateKid($item);
+        });
+        return $kids->all();
     }
 
     public function start(): void
     {
-        foreach ($this->kidsCollection as $kid) {
+        $this->kidsCollection->transform(function ($item) {
             if ($this->time <= 0) {
                 return;
             }
-
             if ($this->time - self::TIME_TO_SCARE < 0) {
                 return;
             }
 
-            if ($this->grinch->fear > $kid->fear) {
-                $this->answer .= $kid->letter;
+            if ($this->grinch->fear > $item->fear) {
+                $this->answer .= $item->letter;
                 $this->time -= self::TIME_TO_SCARE;
-                continue;
+                return;
             }
-
             $this->time -= self::TIME_LOST;
-        }
+
+        });
 
         if (empty($this->answer)) {
             $this->answer = 'GRINCH';
